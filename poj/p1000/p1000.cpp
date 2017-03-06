e85dab807a47f05e0185180f392da44e29447183
ppphp <kevinniub@gmail.com>
2017-03-06 20:35:33 +0800

    first commit

---
+++ poj/p1000/p1000.cpp
@@ -0,0 +1,15 @@
+//
+// Created by dell on 2017/3/6.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<a+b<<endl;
+    return 0;
+}
